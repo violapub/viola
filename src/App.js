0f63863ca91a9328017a58ae66eb0f371c160632
spring-raining <fdonthavei@gmail.com>
2017-07-03 02:29:00 +0900

    Fix a file creation sequence

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import ToolBar from './ToolBar';
 import './App.css';
 
 const projectRoot = '/viola';
-const index = `<!DOCTYPE html>
+const indexTemplate = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Viola index page</title>
   </head>
   <body>
+    <h2>Welcome to Viola</h2>
+    <p>Write your awesome story.</p>
   </body>
 </html>
 `;
@@ -22,32 +24,46 @@ class App extends Component {
 
   ensureFiles = () => {
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const fs = window.Bramble.getFileSystem();
-        console.log(fs);
-        fs.exists(projectRoot, (exists) => {
-          if (!exists) {
-            const sh = new fs.Shell();
-            const Path = window.Bramble.Filer.Path;
-            sh.mkdirp(projectRoot, (err) => {
-              if (err && err.code !== 'EEXIST') {
-                reject(err);
-              }
-            });
-            fs.writeFile(Path.join(projectRoot, ''), index, () => {
-              resolve();
+      const fs = window.Bramble.getFileSystem();
+      const sh = new fs.Shell();
+      const Path = window.Bramble.Filer.Path;
+      const indexPath = Path.join(projectRoot, 'index.html');
+
+      function prepareIndexPage(callback) {
+        fs.exists(indexPath, (exists) => {
+          if (exists) {
+            callback();
+          }
+          else {
+            fs.writeFile(indexPath, indexTemplate, (err) => {
+              if (err) throw err;
+              callback();
             });
           }
-          resolve();
         });
-      }, 1000);
+      }
+
+      try {
+        fs.stat(projectRoot, (err, stats) => {
+          if (err) throw err;
+          if (stats.isDirectory()) {
+            prepareIndexPage(resolve);
+          }
+          sh.mkdirp(projectRoot, (err) => {
+            if (err) throw err;
+            prepareIndexPage(resolve);
+          });
+        });
+      } catch (e) {
+        reject(e);
+      }
     });
   }
 
   componentDidMount() {
     window.Bramble.load('#bramble-root', {
       url: 'http://localhost:8000/dist/index.html',
-      debug: true,
+      debug: false,
       useLocationSearch: true,
     });
 
@@ -61,8 +77,12 @@ class App extends Component {
       })
     });
 
-    this.ensureFiles().then(() => {
-      window.Bramble.mount(projectRoot, 'index.html');
+    window.Bramble.on('readyStateChange', (previous, current) => {
+      if (current === window.Bramble.MOUNTABLE) {
+        this.ensureFiles().then(() => {
+          window.Bramble.mount(projectRoot);
+        });
+      }
     });
   }
 
